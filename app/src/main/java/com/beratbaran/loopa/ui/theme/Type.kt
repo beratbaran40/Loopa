61007a4bc2410f7c1c5272004fd49750b8664f32
Berat Baran <berat_2002_40@hotmail.com>
2025-09-03 19:08:41 +0300

    Refactor: Update typography styles in Type.kt for improved consistency and clarity

--- app/src/main/java/com/beratbaran/loopa/ui/theme/Type.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/theme/Type.kt
@@ -14,121 +14,83 @@ val JakartaFont = FontFamily(
 )
 val Typography = Typography(
 
-    //Onboarding Screen
-
-    titleMedium = TextStyle(
+    displaySmall = TextStyle(
         fontFamily = JakartaFont,
-        fontWeight = FontWeight.SemiBold, //onboarding Screen Logo
+        fontWeight = FontWeight.SemiBold,
         fontSize = 28.sp,
         lineHeight = 30.sp
     ),
 
-    headlineLarge = TextStyle(
+    displayLarge = TextStyle(
         fontFamily = JakartaFont,
-        fontWeight = FontWeight.SemiBold, // onboarding karşılama mesajı
+        fontWeight = FontWeight.SemiBold,
         fontSize = 40.sp,
         lineHeight = 48.sp
     ),
 
-    bodyMedium = TextStyle(
+    bodyLarge = TextStyle(
         fontFamily = JakartaFont,
-        fontWeight = FontWeight.Medium, // onboarding açıklama yazısı - homepage searchbar
+        fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 20.sp
     ),
 
-    labelLarge = TextStyle(
+    bodyMedium = TextStyle(
         fontFamily = JakartaFont,
-        fontWeight = FontWeight.SemiBold, // onboarding buton yazısı - detay sayfasındaki konum detayları
+        fontWeight = FontWeight.SemiBold,
         fontSize = 16.sp,
-        lineHeight = 20.sp,
-
-        ),
-
-    labelSmall = TextStyle(
-        fontFamily = JakartaFont,
-        fontWeight = FontWeight.Medium, // onboarding register/login yazısı // homepage'deki "See All" yazısı
-        fontSize = 14.sp,
-        lineHeight = 18.sp
-    ),
-
-    // homepage/place screen
-
-    headlineMedium = TextStyle(
-        fontFamily = JakartaFont,
-        fontWeight = FontWeight.SemiBold, // Homepage Karşılama mesajı
-        fontSize = 24.sp,
-        lineHeight = 30.sp
-    ),
-
-    labelMedium = TextStyle(
-        fontFamily = JakartaFont,
-        fontWeight = FontWeight.Medium, // homepage durum etiketi
-        fontSize = 12.sp,
-        lineHeight = 16.sp
+        lineHeight = 20.sp
     ),
 
     bodySmall = TextStyle(
         fontFamily = JakartaFont,
-        fontWeight = FontWeight.Medium, // homepage kartlardaki alt bilgi (konum)
-        fontSize = 12.sp,
-        lineHeight = 14.sp
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        lineHeight = 16.sp
     ),
 
     titleLarge = TextStyle(
         fontFamily = JakartaFont,
-        fontWeight = FontWeight.SemiBold, // detay ekranındaki "Overview" başlığı ve "Photos" Başlığı
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+        lineHeight = 28.sp
+    ),
+
+    titleMedium = TextStyle(
+        fontFamily = JakartaFont,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 18.sp
+    ),
+
+    labelMedium = TextStyle(
+        fontFamily = JakartaFont,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 14.sp
+    ),
+
+    titleSmall = TextStyle(
+        fontFamily = JakartaFont,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 20.sp,
         lineHeight = 24.sp
+    ),
 
-    )
+    labelLarge = TextStyle(
+        fontFamily = JakartaFont,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 16.sp
+
+    ),
+
+    labelSmall = TextStyle(
+        fontFamily = JakartaFont,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        lineHeight = 14.sp
+    ),
 )
 
-val LabelLargeFilterTags = Typography.labelLarge
-    .copy(
-        fontSize = 12.sp,       // homepage filtreleme etiketleri
-        lineHeight = 16.sp
-    )
-
-val TitleMediumCardInfo = Typography.titleMedium
-    .copy(
-        fontSize = 14.sp,       // homepage lokasyon kartlarındaki bilgiler
-        lineHeight = 18.sp
-    )
-
-val LabelMediumRating = Typography.labelMedium
-    .copy(
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 12.sp,     // homepage kartlarındaki rating bilgisi
-        lineHeight = 14.sp
-    )
-
-val LabelMediumCardTitle = Typography.labelMedium
-    .copy(
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 20.sp,       // homepage kartlarındaki başlık bilgisi
-        lineHeight = 25.sp
-    )
-
-val TitleMediumSearchResults = Typography.titleMedium
-    .copy(
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 20.sp,       // homepage arama sonuçlarının bilgisi
-        lineHeight = 25.sp
-    )
-
-val HeadlineLargePlaceDetailsTitle = Typography.headlineLarge
-    .copy(
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 24.sp,     // detay ekranındaki konum başlığı
-        lineHeight = 28.sp
-    )
-
-val BodyMediumDetailScreenOverviewDetails = Typography.bodyMedium
-    .copy(
-        fontWeight = FontWeight.Medium,
-        fontSize = 14.sp,     // detay ekranındaki uzun açıklama yazısı
-        lineHeight = 16.sp
-    )
-
-// EN SON TYPOGRAPHY AYARLANDI, LINEHEIGHT VERİLDİ.  / DATA OLUŞTURULACAK. 03.09.2025 - 12.40
+// TYPOGRAPHY DAHA DOĞRU BİR HALE GETİRİLDİ, VERİLER TAMAMLANIYOR... [20/100]. (03.09.2025 - 19.07)
