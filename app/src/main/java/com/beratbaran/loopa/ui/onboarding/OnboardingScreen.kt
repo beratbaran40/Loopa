d3e702d0b7a07d7034c0407dc1ead9f5100b82db
Berat Baran <berat_2002_40@hotmail.com>
2025-09-06 00:57:02 +0300

    Enhance onboarding screen UI with updated background images and improved text styling

--- app/src/main/java/com/beratbaran/loopa/ui/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/onboarding/OnboardingScreen.kt
@@ -25,11 +25,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.beratbaran.loopa.R
@@ -49,8 +53,8 @@ fun OnboardingScreen(
     onNavigateToRegister: () -> Unit,
     onNavigateToLogin: () -> Unit,
 ) {
-    uiEffect.collectWithLifecycle { uiEffect ->
-        when (uiEffect) {
+    uiEffect.collectWithLifecycle { effect ->
+        when (effect) {
             UiEffect.NavigateToLogin -> onNavigateToLogin()
             UiEffect.NavigateToRegister -> onNavigateToRegister()
         }
@@ -63,9 +67,9 @@ fun OnboardingScreen(
     ) {
         val bgImages = listOf(
             R.drawable.eiffel_tower_onboarding_img1,
-            R.drawable.antarctica_onboarding_img2,
+            R.drawable.northen_lights_onboarding_img2,
             R.drawable.maidens_onboarding_img3,
-            R.drawable.serengeti_onboarding_img4
+            R.drawable.africa_onboarding_img4
         )
 
         Crossfade(targetState = uiState.bgIndex, label = "onboarding-bg") { idx ->
@@ -83,14 +87,26 @@ fun OnboardingScreen(
                 .background(color = Color.Black.copy(alpha = 0.5f))
         )
 
+        Image(
+            painter = painterResource(id = R.drawable.loopa),
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 16.dp)
+        )
+
         Text(
             text = stringResource(R.string.app_name),
             modifier = Modifier
                 .align(Alignment.TopCenter)
                 .padding(top = 16.dp)
                 .padding(horizontal = 24.dp),
-            style = MaterialTheme.typography.displaySmall,
-            color = Color.White
+            style = MaterialTheme.typography.displaySmall.copy(
+                brush = Brush.linearGradient(
+                    colors = listOf(Color(0xFFCDFF85), Color.White)
+                )
+            )
+
         )
 
         Column(
@@ -151,10 +167,16 @@ fun OnboardingScreen(
             Spacer(modifier = Modifier.height(16.dp))
 
             Text(
-                text = stringResource(R.string.onboarding_login_text),
+                text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(color = Color.White)) {
+                        append("Already have an account?")
+                    }
+                    withStyle(style = SpanStyle(color = MaterialTheme.colorScheme.primary)) {
+                        append(" Login")
+                    }
+                },
                 modifier = Modifier.clickable { onAction(UiAction.OnLoginClick) },
                 style = MaterialTheme.typography.bodySmall,
-                color = Color.White
             )
         }
     }
