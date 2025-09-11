2d833b849a644c534e3a36ceff7d950d0ab40423
Berat Baran <berat_2002_40@hotmail.com>
2025-09-11 17:35:59 +0300

    feat: Remove background index cycling from LoginViewModel

--- app/src/main/java/com/beratbaran/loopa/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/login/LoginViewModel.kt
@@ -1,18 +1,14 @@
 package com.beratbaran.loopa.ui.login
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.beratbaran.loopa.ui.login.LoginContract.UiAction
 import com.beratbaran.loopa.ui.login.LoginContract.UiEffect
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(
@@ -28,15 +24,6 @@ class LoginViewModel : ViewModel() {
         extraBufferCapacity = 1
     )
     val uiEffect: Flow<UiEffect> = _uiEffect
-
-    init {
-        viewModelScope.launch {
-            while (isActive) {
-                delay(2000)
-                _uiState.update { it.copy(bgIndex = (it.bgIndex + 1) % 4) }
-            }
-        }
-    }
 
     fun onAction(uiAction: UiAction) {
         when (uiAction) {
