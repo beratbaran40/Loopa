ff4490eecf7bcedf7cfd8271ae86bb6a09f18c36
Berat Baran <berat_2002_40@hotmail.com>
2025-09-14 16:39:44 +0300

    feat: Refactor LoginContract to enhance UI state management and action handling

--- app/src/main/java/com/beratbaran/loopa/ui/login/LoginContract.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/login/LoginContract.kt
@@ -2,20 +2,27 @@ package com.beratbaran.loopa.ui.login
 
 object LoginContract {
     data class UiState(
-        val bgIndex: Int = 0,
         val email: String = "",
         val password: String = "",
-        val isLoading: Boolean,
-        val errorMessages: String?,
+        val isLoading: Boolean = false,
+        val errorMessage: String?,
+        val emailEdited: Boolean = false,
+        val submitAttempted: Boolean = false,
+        val showPassword: Boolean = false,
+        val isEmailValid: Boolean = true,
+        val supportingText: String? = null
     )
 
     sealed interface UiAction {
-        data object ClickedToLogin : UiAction
-        data class EmailChanged(val email: String) : UiAction
-        data class PasswordChanged(val password: String) : UiAction
+        data object OnLoginClicked : UiAction
+        data class OnEmailChange(val email: String) : UiAction
+        data class OnPasswordChange(val password: String) : UiAction
+        data object OnToggleShowPassword : UiAction
+        data object OnSubmitAttempted : UiAction
     }
 
     sealed interface UiEffect {
         data object NavigateToHomePage : UiEffect
+
     }
 }
