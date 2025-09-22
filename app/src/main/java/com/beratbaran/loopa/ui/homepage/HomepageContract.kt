38bcabd3f2d751fa06215b55f7e35bd16fddec44
Berat Baran <berat_2002_40@hotmail.com>
2025-09-22 19:43:38 +0300

    feat: Add name, topPlaces, and wannaLookPlaces to HomepageContract.UiState

--- app/src/main/java/com/beratbaran/loopa/ui/homepage/HomepageContract.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/homepage/HomepageContract.kt
@@ -3,6 +3,9 @@ package com.beratbaran.loopa.ui.homepage
 object HomepageContract {
 
     data class UiState(
+        val name: String = "",
+        val topPlaces: List<PlaceModel> = emptyList(),
+        val wannaLookPlaces: List<PlaceModel> = emptyList(),
         val isLoading: Boolean = false,
     )
 
