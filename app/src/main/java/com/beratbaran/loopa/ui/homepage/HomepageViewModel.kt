3555d213025db3e104a320e7b0301f500d673124
Berat Baran <berat_2002_40@hotmail.com>
2025-09-22 14:39:15 +0300

    feat: Create HomepageViewModel with basic UI state and actions

---
+++ app/src/main/java/com/beratbaran/loopa/ui/homepage/HomepageViewModel.kt
@@ -0,0 +1,28 @@
+package com.beratbaran.loopa.ui.homepage
+
+import androidx.lifecycle.ViewModel
+import com.beratbaran.loopa.ui.homepage.HomepageContract.UiAction
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+
+class HomepageViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(
+        HomepageContract.UiState(
+        )
+    )
+
+    val uiState: StateFlow<HomepageContract.UiState> = _uiState.asStateFlow()
+
+
+    private val _uiEffect by lazy { Channel<HomepageContract.UiEffect>() }
+    val uiEffect: Flow<HomepageContract.UiEffect> by lazy { _uiEffect.receiveAsFlow() }
+
+    fun onAction(uiAction: UiAction) {
+
+    }
+
+}
