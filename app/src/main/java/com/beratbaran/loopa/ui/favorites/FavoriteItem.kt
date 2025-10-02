30a470fa2cd4e1fe85901ac4071ce4bbaa17c18c
Berat Baran <berat_2002_40@hotmail.com>
2025-10-03 01:59:58 +0300

    feat: Implement UI for FavoriteItem composable

--- app/src/main/java/com/beratbaran/loopa/ui/favorites/FavoriteItem.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/favorites/FavoriteItem.kt
@@ -1,7 +1,37 @@
 package com.beratbaran.loopa.ui.favorites
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.beratbaran.loopa.R
 import com.beratbaran.loopa.ui.theme.MyappTheme
 
 @Composable
@@ -11,23 +41,136 @@ fun FavoriteItem(
     imageUrl: String,
     rating: String,
     isFavorite: Boolean,
-    onFavoriteClick: () -> Unit,
+    onUnFavoriteClick: () -> Unit,
     onDetailsClick: () -> Unit,
 ) {
 
+    Card(
+        modifier = Modifier.widthIn(max = 200.dp),
+        onClick = onDetailsClick,
+        colors = CardDefaults.cardColors(MaterialTheme.colorScheme.primaryContainer),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = RoundedCornerShape(16.dp),
+        border = BorderStroke(2.dp, MaterialTheme.colorScheme.outlineVariant)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(180.dp),
+            ) {
+                AsyncImage(
+                    model = imageUrl,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    placeholder = painterResource(R.drawable.top_place_item_img),
+                )
+
+                IconButton(
+                    onClick = onUnFavoriteClick,
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(16.dp)
+                        .size(32.dp)
+                        .background(
+                            color = MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.40f),
+                            shape = CircleShape,
+                        ),
+                ) {
+                    Icon(
+                        modifier = Modifier
+                            .size(30.dp),
+                        painter = painterResource(
+                            id = if (isFavorite) R.drawable.ic_selected_favorite
+                            else R.drawable.ic_favorite
+                        ),
+                        contentDescription = "Favorite",
+                        tint = MaterialTheme.colorScheme.primary,
+                    )
+                }
+            }
+
+            Text(
+                modifier = Modifier.padding(horizontal = 12.dp),
+                text = name,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.SemiBold,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+
+            Row(
+                modifier = Modifier
+                    .padding(horizontal = 10.dp)
+                    .padding(bottom = 12.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(14.dp),
+                    painter = painterResource(R.drawable.ic_location),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary,
+
+                    )
+
+                Spacer(modifier = Modifier.width(2.dp))
+
+                Text(
+                    text = location,
+                    style = MaterialTheme.typography.labelMedium,
+                    fontWeight = FontWeight.Medium,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+
+                Spacer(modifier = Modifier.width(6.dp))
+
+                Icon(
+                    modifier = Modifier
+                        .size(14.dp),
+                    painter = painterResource(R.drawable.ic_star),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.secondary
+                )
+
+                Spacer(modifier = Modifier.width(4.dp))
+
+                Text(
+                    text = rating,
+                    style = MaterialTheme.typography.labelMedium,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+            }
+        }
+    }
 }
 
 @PreviewLightDark
 @Composable
-fun FavoriteItemPreview() {
+fun FavoriteItemPreview(
+    name: String = "Eiffel Tower",
+    location: String = "Location",
+    imageUrl: String = "Image",
+    rating: String = "4.5",
+    isFavorite: Boolean = false,
+) {
     MyappTheme {
         FavoriteItem(
-            name = "Eiffel Tower",
-            location = "Paris, France",
-            imageUrl = "",
-            rating = "4.8",
-            isFavorite = true,
-            onFavoriteClick = {},
+            name = name,
+            location = location,
+            imageUrl = imageUrl,
+            rating = rating,
+            isFavorite = isFavorite,
+            onUnFavoriteClick = {},
             onDetailsClick = {},
         )
     }
