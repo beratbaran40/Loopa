7b7cb253b3fd15457e60d27f47ed8296eb3bc09c
Berat Baran <berat_2002_40@hotmail.com>
2025-10-03 01:59:36 +0300

    feat: Update FavoritesContract for unfavorite action
    Refactor `FavoritesContract`:
    - Add `isFavorite` state to `UiState`
    - Rename `favorites` to `favoriteItem` in `UiState`
    - Remove `NavigateToBack` effect
    - Replace `OnBackClick` with `OnUnFavoriteClick` action

--- app/src/main/java/com/beratbaran/loopa/ui/favorites/FavoritesContract.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/favorites/FavoritesContract.kt
@@ -2,18 +2,18 @@ package com.beratbaran.loopa.ui.favorites
 
 object FavoritesContract {
     data class UiState(
+        val isFavorite: Boolean = true,
         val isLoading: Boolean = false,
-        val favorites: List<FavoritesModel> = emptyList(),
+        val favoriteItem: List<FavoritesModel> = emptyList(),
     )
 
     sealed class UiEffect {
         data object NavigateToDetails : UiEffect()
-        data object NavigateToBack : UiEffect()
     }
 
     sealed class UiAction {
         data object OnDetailsClick : UiAction()
-        data object OnBackClick : UiAction()
+        data object OnUnFavoriteClick : UiAction()
     }
 }
 
