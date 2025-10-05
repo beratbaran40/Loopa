a7fe24fe8c9b6be7d5d470a7690f7df4537205b3
Berat Baran <berat_2002_40@hotmail.com>
2025-10-05 23:30:19 +0300

    feat: Add SearchScreenPreviewProvider for UI state previews

---
+++ app/src/main/java/com/beratbaran/loopa/ui/search/SearchScreenPreviewProvider.kt
@@ -0,0 +1,52 @@
+package com.beratbaran.loopa.ui.search
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.beratbaran.loopa.ui.homepage.PlaceModel
+
+class SearchScreenPreviewProvider : PreviewParameterProvider<SearchContract.UiState> {
+    override val values: Sequence<SearchContract.UiState>
+        get() = sequenceOf(
+            SearchContract.UiState(
+                isLoading = true,
+            ),
+            SearchContract.UiState(
+                isLoading = false,
+                places = listOf(
+                    PlaceModel(
+                        name = "Bryce Canyon National Park",
+                        location = "Location",
+                        imageUrl = "Image",
+                        rating = "4.5",
+                        isFavorite = false,
+                        id = 1
+                    ),
+                    PlaceModel(
+                        name = "Bryce Canyon National Park",
+                        location = "Location",
+                        imageUrl = "Image",
+                        rating = "4.5",
+                        isFavorite = true,
+                        id = 2
+                    ),
+                    PlaceModel(
+                        name = "Bryce Canyon National Park",
+                        location = "Location",
+                        imageUrl = "Image",
+                        rating = "4.5",
+                        isFavorite = false,
+                        id = 3
+                    ),
+                    PlaceModel(
+                        name = "Bryce Canyon National Park",
+                        location = "Location",
+                        imageUrl = "Image",
+                        rating = "4.5",
+                        isFavorite = true,
+                        id = 4
+
+                    ),
+                )
+            )
+        )
+
+}
