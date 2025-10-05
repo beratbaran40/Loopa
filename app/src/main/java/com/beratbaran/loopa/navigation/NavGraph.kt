de1922eeb6324342f1de8ee5bddf56d0f33f6a0e
Berat Baran <berat_2002_40@hotmail.com>
2025-10-05 23:55:18 +0300

    feat: Update navigation from SearchScreen to Homepage in NavGraph

--- app/src/main/java/com/beratbaran/loopa/navigation/NavGraph.kt
+++ app/src/main/java/com/beratbaran/loopa/navigation/NavGraph.kt
@@ -93,7 +93,7 @@ fun NavGraph(
                 uiEffect = viewModel.uiEffect,
                 onAction = viewModel::onAction,
                 onNavigateToHomepage = {
-                    navController.navigate(Screen.SearchScreen) {
+                    navController.navigate(Screen.Homepage) {
                         popUpTo(Screen.Login) {
                             inclusive = true
                         }
