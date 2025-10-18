a59020690647fd9fd0a8962bb992d2907b5fa94c
Berat Baran <berat_2002_40@hotmail.com>
2025-10-18 03:29:55 +0300

    feat: Add ProfileAvatar composable for user profile display

---
+++ app/src/main/java/com/beratbaran/loopa/ui/profile/ProfileAvatar.kt
@@ -0,0 +1,71 @@
+package com.beratbaran.loopa.ui.profile
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.beratbaran.loopa.R
+import com.beratbaran.loopa.ui.theme.MyappTheme
+
+@Composable
+fun ProfileAvatar(
+    imageUrl: String,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 32.dp, start = 16.dp, end = 16.dp)
+    ) {
+
+        AsyncImage(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .size(120.dp)
+                .clip(CircleShape),
+            model = imageUrl,
+            contentDescription = null,
+            placeholder = painterResource(R.drawable.ic_avatar),
+            error = painterResource(R.drawable.ic_error),
+            contentScale = ContentScale.Crop,
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurfaceVariant),
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = stringResource(R.string.profile_screen_profile_name),
+            style = MaterialTheme.typography.displayLarge,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun ProfileAvatarPreview(
+    imageUrl: String = "Profile Avatar",
+) {
+    MyappTheme {
+        ProfileAvatar(
+            imageUrl = imageUrl,
+        )
+    }
+}
