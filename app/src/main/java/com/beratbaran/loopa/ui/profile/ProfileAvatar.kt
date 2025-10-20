3129a8ade786e3e649caeb35c5c68ed25489db9d
Berat Baran <berat_2002_40@hotmail.com>
2025-10-20 19:29:00 +0300

    fix: Remove color filter from ProfileAvatar
    The `ColorFilter.tint` applied to the `AsyncImage` in the `ProfileAvatar` composable has been removed. This allows the avatar image to be displayed in its original colors instead of being tinted with the `onSurfaceVariant` color.

--- app/src/main/java/com/beratbaran/loopa/ui/profile/ProfileAvatar.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/profile/ProfileAvatar.kt
@@ -15,7 +15,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -38,7 +37,7 @@ fun ProfileAvatar(
         Box(
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
-                .clickable {  }
+                .clickable { }
         ) {
 
             AsyncImage(
@@ -50,7 +49,6 @@ fun ProfileAvatar(
                 placeholder = painterResource(R.drawable.ic_avatar),
                 error = painterResource(R.drawable.ic_error),
                 contentScale = ContentScale.Crop,
-                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurfaceVariant),
             )
 
         }
