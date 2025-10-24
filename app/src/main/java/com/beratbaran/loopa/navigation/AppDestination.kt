b8f33b5ea3f13d51a384e146ebbc1074ff876f78
Berat Baran <berat_2002_40@hotmail.com>
2025-10-24 23:23:00 +0300

    refactor: Capitalize route names in AppDestination
    The route strings for all sealed `AppDestination` objects have been changed from lowercase to capitalized case to follow a consistent naming convention.

--- app/src/main/java/com/beratbaran/loopa/navigation/AppDestination.kt
+++ app/src/main/java/com/beratbaran/loopa/navigation/AppDestination.kt
@@ -5,35 +5,35 @@ import com.beratbaran.loopa.R
 sealed class AppDestination(val title: Int, val route: String, val icon: Int, val selectedIcon: Int) {
     data object Home : AppDestination(
         title = R.string.navbar_home_screen_page_name,
-        route = "home",
+        route = "Home",
         icon = R.drawable.ic_home,
         selectedIcon = R.drawable.ic_selected_home
     )
 
     data object Categories : AppDestination(
         title = R.string.navbar_categories_screen_page_name,
-        route = "categories",
+        route = "Categories",
         icon = R.drawable.ic_category,
         selectedIcon = R.drawable.ic_selected_category
     )
 
     data object Search : AppDestination(
         title = R.string.navbar_search_screen_page_name,
-        route = "search",
+        route = "Search",
         icon = R.drawable.ic_search,
         selectedIcon = R.drawable.ic_selected_search
     )
 
     data object Favorites : AppDestination(
         title = R.string.navbar_favorites_screen_page_name,
-        route = "favorites",
+        route = "Favorites",
         icon = R.drawable.ic_favorite,
         selectedIcon = R.drawable.ic_selected_favorite
     )
 
     data object Profile : AppDestination(
         title = R.string.navbar_profile_screen_page_name,
-        route = "profile",
+        route = "Profile",
         icon = R.drawable.ic_profile,
         selectedIcon = R.drawable.ic_selected_profile
     )
