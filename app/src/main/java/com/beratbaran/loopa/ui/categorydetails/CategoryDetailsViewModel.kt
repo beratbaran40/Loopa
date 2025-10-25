479be58e05dfcd560b059edbef3340abc6de1d0e
Berat Baran <berat_2002_40@hotmail.com>
2025-10-25 16:59:38 +0300

    refactor: Refactor CategoryDetailsViewModel to use BaseViewModel
    The CategoryDetailsViewModel is updated to extend the new `BaseViewModel`. This change removes the manual implementation of `Channel` for handling UI effects and instead uses the `setEffect` method provided by the base class.

--- app/src/main/java/com/beratbaran/loopa/ui/categorydetails/CategoryDetailsViewModel.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/categorydetails/CategoryDetailsViewModel.kt
@@ -1,24 +1,18 @@
 package com.beratbaran.loopa.ui.categorydetails
 
-import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.Flow
+import com.beratbaran.loopa.ui.base.BaseViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
 
-class CategoryDetailsViewModel : ViewModel() {
+class CategoryDetailsViewModel : BaseViewModel<CategoryDetailsContract.UiEffect>() {
     private val _uiState = MutableStateFlow(CategoryDetailsContract.UiState())
     val uiState: StateFlow<CategoryDetailsContract.UiState> = _uiState.asStateFlow()
-
-    private val _uiEffect by lazy { Channel<CategoryDetailsContract.UiEffect>() }
-    val uiEffect: Flow<CategoryDetailsContract.UiEffect> by lazy { _uiEffect.receiveAsFlow() }
 
     fun onAction(action: CategoryDetailsContract.UiAction) {
         when (action) {
             CategoryDetailsContract.UiAction.OnDetailsClick -> {
-                _uiEffect.trySend(CategoryDetailsContract.UiEffect.NavigateToDetails)
+                setEffect(CategoryDetailsContract.UiEffect.NavigateToDetails)
             }
 
             CategoryDetailsContract.UiAction.OnToggleFavorite -> {
@@ -26,7 +20,7 @@ class CategoryDetailsViewModel : ViewModel() {
             }
 
             CategoryDetailsContract.UiAction.OnBackClick -> {
-                _uiEffect.trySend(CategoryDetailsContract.UiEffect.NavigateToBack)
+                setEffect(CategoryDetailsContract.UiEffect.NavigateToBack)
             }
         }
     }
