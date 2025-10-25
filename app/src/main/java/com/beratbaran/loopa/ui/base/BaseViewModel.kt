ea92400fe74b4d071706ace73443dfb325361773
Berat Baran <berat_2002_40@hotmail.com>
2025-10-25 16:58:35 +0300

    feat: Explicitly define Flow type for uiEffect
    Explicitly sets the type of `uiEffect` to `Flow<E>` in the `BaseViewModel`. This improves code readability and type safety.

--- app/src/main/java/com/beratbaran/loopa/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/base/BaseViewModel.kt
@@ -3,13 +3,14 @@ package com.beratbaran.loopa.ui.base
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
-abstract class BaseViewModel<E: Any>: ViewModel() {
+abstract class BaseViewModel<E : Any> : ViewModel() {
 
     protected val _uiEffect = Channel<E>(capacity = Channel.BUFFERED)
-    val uiEffect = _uiEffect.receiveAsFlow()
+    val uiEffect: Flow<E> = _uiEffect.receiveAsFlow()
 
     protected fun setEffect(effect: E) {
         viewModelScope.launch {
