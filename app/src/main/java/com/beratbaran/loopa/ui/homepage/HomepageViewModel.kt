24b3bae3f9350b8d4d00084b62c720a3eda33166
Berat Baran <berat_2002_40@hotmail.com>
2025-10-25 16:59:19 +0300

    refactor: Make HomepageViewModel extend BaseViewModel
    The `HomepageViewModel` is updated to inherit from the new `BaseViewModel<HomepageContract.UiEffect>`.
    
    This change simplifies the view model by removing the manual implementation of `Channel` for UI effects and instead uses the `setEffect` method provided by the base class. The `onAction` function is updated accordingly to use `setEffect`.

--- app/src/main/java/com/beratbaran/loopa/ui/homepage/HomepageViewModel.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/homepage/HomepageViewModel.kt
@@ -1,30 +1,20 @@
 package com.beratbaran.loopa.ui.homepage
 
-import androidx.lifecycle.ViewModel
+import com.beratbaran.loopa.ui.base.BaseViewModel
 import com.beratbaran.loopa.ui.homepage.HomepageContract.UiAction
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
 
-class HomepageViewModel : ViewModel() {
+class HomepageViewModel : BaseViewModel<HomepageContract.UiEffect>() {
+
     private val _uiState = MutableStateFlow(HomepageContract.UiState())
     val uiState: StateFlow<HomepageContract.UiState> = _uiState.asStateFlow()
 
-    private val _uiEffect by lazy { Channel<HomepageContract.UiEffect>() }
-    val uiEffect: Flow<HomepageContract.UiEffect> by lazy { _uiEffect.receiveAsFlow() }
-
     fun onAction(uiAction: UiAction) {
         when (uiAction) {
-            UiAction.OnDetailsClick -> {
-                _uiEffect.trySend(HomepageContract.UiEffect.NavigateToDetails)
-            }
-
-            UiAction.ToggleFavorite -> {
-                _uiEffect.trySend(HomepageContract.UiEffect.NavigateToFavorites)
-            }
+            UiAction.OnDetailsClick -> setEffect(HomepageContract.UiEffect.NavigateToDetails)
+            UiAction.ToggleFavorite -> setEffect(HomepageContract.UiEffect.NavigateToFavorites)
         }
     }
 }
