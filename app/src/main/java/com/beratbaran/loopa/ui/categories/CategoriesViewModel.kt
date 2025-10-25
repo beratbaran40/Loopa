96b1400bd0e1d77e22b6c74c474a8446a4029593
Berat Baran <berat_2002_40@hotmail.com>
2025-10-25 16:59:31 +0300

    refactor: Update CategoriesViewModel to use BaseViewModel
    This commit refactors `CategoriesViewModel` to extend the new `BaseViewModel`. It replaces the manual implementation of a `Channel` for UI effects with the `setEffect` method provided by the base class.

--- app/src/main/java/com/beratbaran/loopa/ui/categories/CategoriesViewModel.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/categories/CategoriesViewModel.kt
@@ -1,20 +1,14 @@
 package com.beratbaran.loopa.ui.categories
 
-import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.Flow
+import com.beratbaran.loopa.ui.base.BaseViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
 
-class CategoriesViewModel : ViewModel() {
+class CategoriesViewModel : BaseViewModel<CategoriesContract.UiEffect>() {
 
     private val _uiState = MutableStateFlow(CategoriesContract.UiState())
     val uiState: StateFlow<CategoriesContract.UiState> = _uiState.asStateFlow()
-
-    private val _uiEffect by lazy { Channel<CategoriesContract.UiEffect>() }
-    val uiEffect: Flow<CategoriesContract.UiEffect> by lazy { _uiEffect.receiveAsFlow() }
 
     init {
         _uiState.value = CategoriesContract.UiState(
@@ -25,7 +19,7 @@ class CategoriesViewModel : ViewModel() {
     fun onAction(uiAction: CategoriesContract.UiAction) {
         when (uiAction) {
             CategoriesContract.UiAction.OnCategoryClick -> {
-                _uiEffect.trySend(CategoriesContract.UiEffect.NavigateToCategoryDetails)
+                setEffect (CategoriesContract.UiEffect.NavigateToCategoryDetails)
             }
         }
     }
