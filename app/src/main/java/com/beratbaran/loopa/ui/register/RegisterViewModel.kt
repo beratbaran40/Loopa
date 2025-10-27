e9c089ac923a3fa04270b4990d25e8bbe137153a
Berat Baran <berat_2002_40@hotmail.com>
2025-10-27 18:31:16 +0300

    refactor: Update RegisterViewModel to use generic BaseViewModel
    Refactors `RegisterViewModel` to extend the new generic `BaseViewModel<UiState, UiEffect>`. This change simplifies state management by removing the local `_uiState` `MutableStateFlow` and adopting the `setState` and `uiState` properties from the base class.

--- app/src/main/java/com/beratbaran/loopa/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/register/RegisterViewModel.kt
@@ -4,19 +4,10 @@ import com.beratbaran.loopa.common.ValidationManager
 import com.beratbaran.loopa.ui.base.BaseViewModel
 import com.beratbaran.loopa.ui.register.RegisterContract.PasswordStrength
 import com.beratbaran.loopa.ui.register.RegisterContract.UiAction
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 
-class RegisterViewModel : BaseViewModel<RegisterContract.UiEffect>() {
-    private val _uiState = MutableStateFlow(
-        RegisterContract.UiState(
-        )
-    )
-
-    val uiState: StateFlow<RegisterContract.UiState> = _uiState.asStateFlow()
-
+class RegisterViewModel : BaseViewModel<RegisterContract.UiState, RegisterContract.UiEffect>(
+    initialState = RegisterContract.UiState()
+) {
     private fun computePasswordStrength(password: String): PasswordStrength? {
         if (password.isBlank()) return null
 
@@ -57,7 +48,7 @@ class RegisterViewModel : BaseViewModel<RegisterContract.UiEffect>() {
     fun onAction(uiAction: UiAction) {
         when (uiAction) {
             UiAction.OnRegisterClick -> {
-                val currentState = _uiState.value
+                val currentState = uiState.value
                 val nameError = ValidationManager.validateName(currentState.name)
                 val surnameError = ValidationManager.validateSurname(currentState.surname)
                 val emailError = ValidationManager.validateEmail(currentState.email)
@@ -72,8 +63,8 @@ class RegisterViewModel : BaseViewModel<RegisterContract.UiEffect>() {
                 ) {
                     setEffect(RegisterContract.UiEffect.NavigateToHomePage)
                 } else {
-                    _uiState.update {
-                        it.copy(
+                    setState {
+                        copy(
                             supportingTextName = nameError,
                             supportingTextSurname = surnameError,
                             supportingTextEmail = emailError,
@@ -83,65 +74,65 @@ class RegisterViewModel : BaseViewModel<RegisterContract.UiEffect>() {
                 }
             }
 
-            is UiAction.OnNameChange -> _uiState.update {
-                it.copy(
+            is UiAction.OnNameChange -> setState {
+                copy(
                     name = uiAction.name,
                     isRegisterEnabled = checkRegisterState(
-                        uiAction.name, it.surname, it.email, it.password,
+                        uiAction.name, surname, email, password,
                     )
                 )
             }
 
-            is UiAction.OnSurnameChange -> _uiState.update {
-                it.copy(
+            is UiAction.OnSurnameChange -> setState {
+                copy(
                     surname = uiAction.surname,
                     isRegisterEnabled = checkRegisterState(
-                        it.name, uiAction.surname, it.email, it.password,
+                        name, uiAction.surname, email, password,
                     )
                 )
             }
 
-            is UiAction.OnEmailChange -> _uiState.update {
-                it.copy(
+            is UiAction.OnEmailChange -> setState {
+                copy(
                     email = uiAction.email,
                     isRegisterEnabled = checkRegisterState(
-                        it.name, it.surname, uiAction.email, it.password,
+                        name, surname, uiAction.email, password,
                     )
                 )
             }
 
-            is UiAction.OnPasswordChange -> _uiState.update {
+            is UiAction.OnPasswordChange -> setState {
                 val newPassword = uiAction.password
-                it.copy(
+                copy(
                     password = newPassword,
                     isRegisterEnabled = checkRegisterState(
-                        name = it.name,
-                        surname = it.surname,
-                        email = it.email,
+                        name = name,
+                        surname = surname,
+                        email = email,
                         password = newPassword,
                     ),
                     passwordStrength = computePasswordStrength(newPassword),
                 )
             }
 
-            is UiAction.OnNameTextFieldFocusChange -> _uiState.update {
-                it.copy(isNameTextFieldFocused = uiAction.isFocused)
+            is UiAction.OnNameTextFieldFocusChange -> setState {
+                copy(isNameTextFieldFocused = uiAction.isFocused)
             }
 
-            is UiAction.OnSurnameTextFieldFocusChange -> _uiState.update {
-                it.copy(isSurnameTextFieldFocused = uiAction.isFocused)
+            is UiAction.OnSurnameTextFieldFocusChange -> setState {
+                copy(isSurnameTextFieldFocused = uiAction.isFocused)
             }
 
-            is UiAction.OnEmailTextFieldFocusChange -> _uiState.update {
-                it.copy(isEmailTextFieldFocused = uiAction.isFocused)
+            is UiAction.OnEmailTextFieldFocusChange -> setState {
+                copy(isEmailTextFieldFocused = uiAction.isFocused)
             }
 
-            is UiAction.OnPasswordTextFieldFocusChange -> _uiState.update {
-                it.copy(isPasswordTextFieldFocused = uiAction.isFocused)
+            is UiAction.OnPasswordTextFieldFocusChange -> setState {
+                copy(isPasswordTextFieldFocused = uiAction.isFocused)
             }
 
-            UiAction.OnToggleShowPassword -> _uiState.update {
-                it.copy(showPassword = !it.showPassword)
+            UiAction.OnToggleShowPassword -> setState {
+                copy(showPassword = !showPassword)
             }
 
             UiAction.OnBackClick -> setEffect(RegisterContract.UiEffect.NavigateToBack)
