00a75aabe298bf56f8f615c38c535adc82efbadc
Berat Baran <berat_2002_40@hotmail.com>
2025-10-27 18:20:13 +0300

    refactor: Enhance BaseViewModel with state management using StateFlow
    This commit introduces `StateFlow` for managing UI state within `BaseViewModel`.
    
    - It adds a generic state type `S` to the `BaseViewModel`.
    - A `MutableStateFlow` (`_uiState`) is initialized to hold the current state.
    - A public, read-only `StateFlow` (`uiState`) is exposed for observers.
    - A `setState` function is added to provide a structured way to update the state.

--- app/src/main/java/com/beratbaran/loopa/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/base/BaseViewModel.kt
@@ -4,12 +4,22 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-abstract class BaseViewModel<E : Any> : ViewModel() {
+abstract class BaseViewModel<S: Any, E: Any>(initialState: S): ViewModel() {
+    private val _uiState = MutableStateFlow(initialState)
+    val uiState: StateFlow<S> = _uiState.asStateFlow()
 
-    protected val _uiEffect = Channel<E>(capacity = Channel.BUFFERED)
+    protected fun setState(reducer: S.() -> S) {
+        _uiState.update { it.reducer() }
+    }
+
+    private val _uiEffect = Channel<E>(capacity = Channel.BUFFERED)
     val uiEffect: Flow<E> = _uiEffect.receiveAsFlow()
 
     protected fun setEffect(effect: E) {
