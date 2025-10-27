f34fbf88688964a130a28d45e91dacfd02e28093
Berat Baran <berat_2002_40@hotmail.com>
2025-10-27 18:20:47 +0300

    refactor: Update CategoriesViewModel to use new BaseViewModel
    The `CategoriesViewModel` is updated to inherit from the new `BaseViewModel` which now manages the `UiState`. This change simplifies the ViewModel by removing the manual management of `MutableStateFlow` and `StateFlow` for the UI state. State initialization is now handled through the `setState` function provided by the base class.

--- app/src/main/java/com/beratbaran/loopa/ui/categories/CategoriesViewModel.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/categories/CategoriesViewModel.kt
@@ -1,25 +1,22 @@
 package com.beratbaran.loopa.ui.categories
 
 import com.beratbaran.loopa.ui.base.BaseViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 
-class CategoriesViewModel : BaseViewModel<CategoriesContract.UiEffect>() {
-
-    private val _uiState = MutableStateFlow(CategoriesContract.UiState())
-    val uiState: StateFlow<CategoriesContract.UiState> = _uiState.asStateFlow()
-
+class CategoriesViewModel : BaseViewModel<CategoriesContract.UiState, CategoriesContract.UiEffect>(
+    initialState = CategoriesContract.UiState()
+) {
     init {
-        _uiState.value = CategoriesContract.UiState(
-            categories = loopaCategories(),
-        )
+        setState {
+            copy(
+                categories = loopaCategories()
+            )
+        }
     }
 
     fun onAction(uiAction: CategoriesContract.UiAction) {
         when (uiAction) {
             CategoriesContract.UiAction.OnCategoryClick -> {
-                setEffect (CategoriesContract.UiEffect.NavigateToCategoryDetails)
+                setEffect(CategoriesContract.UiEffect.NavigateToCategoryDetails)
             }
         }
     }
