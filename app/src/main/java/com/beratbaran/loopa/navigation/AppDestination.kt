376b8901258839b42ec8083a0232af118714a359
Berat Baran <berat_2002_40@hotmail.com>
2025-11-08 15:16:53 +0300

    refactor: Update route definitions to use qualified names for navigation

--- app/src/main/java/com/beratbaran/loopa/navigation/AppDestination.kt
+++ app/src/main/java/com/beratbaran/loopa/navigation/AppDestination.kt
@@ -5,35 +5,35 @@ import com.beratbaran.loopa.R
 sealed class AppDestination(val title: Int, val route: String, val icon: Int, val selectedIcon: Int) {
     data object Home : AppDestination(
         title = R.string.navbar_home_screen_page_name,
-        route = "Home",
+        route = Screen.Homepage::class.qualifiedName!!,
         icon = R.drawable.ic_home,
         selectedIcon = R.drawable.ic_selected_home
     )
 
     data object Categories : AppDestination(
         title = R.string.navbar_categories_screen_page_name,
-        route = "Categories",
+        route = Screen.CategoriesScreen::class.qualifiedName!!,
         icon = R.drawable.ic_category,
         selectedIcon = R.drawable.ic_selected_category
     )
 
     data object Search : AppDestination(
         title = R.string.navbar_search_screen_page_name,
-        route = "Search",
+        route = Screen.SearchScreen::class.qualifiedName!!,
         icon = R.drawable.ic_search,
         selectedIcon = R.drawable.ic_selected_search
     )
 
     data object Favorites : AppDestination(
         title = R.string.navbar_favorites_screen_page_name,
-        route = "Favorites",
+        route = Screen.FavoritesScreen::class.qualifiedName!!,
         icon = R.drawable.ic_favorite,
         selectedIcon = R.drawable.ic_selected_favorite
     )
 
     data object Profile : AppDestination(
         title = R.string.navbar_profile_screen_page_name,
-        route = "Profile",
+        route = Screen.ProfileScreen::class.qualifiedName!!,
         icon = R.drawable.ic_profile,
         selectedIcon = R.drawable.ic_selected_profile
     )
