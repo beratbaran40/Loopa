9856c332d4b73668657e9f86b3cc43224e71e530
Berat Baran <berat_2002_40@hotmail.com>
2025-12-05 16:21:17 +0300

    feat: Update LoopaApi to enhance response models and add place detail retrieval

--- app/src/main/java/com/beratbaran/loopa/data/remote/api/LoopaApi.kt
+++ app/src/main/java/com/beratbaran/loopa/data/remote/api/LoopaApi.kt
@@ -1,25 +1,35 @@
 package com.beratbaran.loopa.data.remote.api
 
-import com.beratbaran.loopa.data.model.CategoryDto
+import com.beratbaran.loopa.data.model.CategoryResponseDto
 import com.beratbaran.loopa.data.model.FavoriteDto
-import com.beratbaran.loopa.data.model.PlaceDto
+import com.beratbaran.loopa.data.model.PlaceDetailsResponseDto
+import com.beratbaran.loopa.data.model.PlaceResponseDto
 import com.beratbaran.loopa.data.model.RegisterRequest
 import com.beratbaran.loopa.data.model.RegisterResponse
 import com.beratbaran.loopa.data.model.UserDto
+import com.beratbaran.loopa.data.model.UserResponseDto
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface LoopaApi {
-    @GET("users")
-    suspend fun getUsers(): List<UserDto>
-
     @GET("places")
-    suspend fun getPlaces(): List<PlaceDto>
+    suspend fun getPlaces(): PlaceResponseDto
+
+    @GET("users")
+    suspend fun getUser(@Query("uid") uid: String): UserResponseDto
+
+
+    @GET("places/{placeId}")
+    suspend fun getPlaceDetail(
+        @Path("placeId") placeId: Int,
+    ): PlaceDetailsResponseDto
 
     @GET("categories")
-    suspend fun getCategories(): List<CategoryDto>
+    suspend fun getCategories(): CategoryResponseDto
 
     @GET("favorites")
     suspend fun getFavorites(): List<FavoriteDto>
