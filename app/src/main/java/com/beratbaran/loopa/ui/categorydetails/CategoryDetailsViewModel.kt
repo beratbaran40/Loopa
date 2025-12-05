f58e3d78c675bebd34136c99dca33341b6b64aea
Berat Baran <berat_2002_40@hotmail.com>
2025-12-05 16:33:27 +0300

    feat: Refactor CategoryDetailsViewModel to use Hilt for dependency injection and add categoryId handling

--- app/src/main/java/com/beratbaran/loopa/ui/categorydetails/CategoryDetailsViewModel.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/categorydetails/CategoryDetailsViewModel.kt
@@ -1,17 +1,35 @@
 package com.beratbaran.loopa.ui.categorydetails
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
+import com.beratbaran.loopa.data.repository.PlaceRepository
 import com.beratbaran.loopa.ui.base.BaseViewModel
 import com.beratbaran.loopa.ui.categorydetails.CategoryDetailsContract.UiAction
 import com.beratbaran.loopa.ui.categorydetails.CategoryDetailsContract.UiEffect
 import com.beratbaran.loopa.ui.categorydetails.CategoryDetailsContract.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class CategoryDetailsViewModel :
-    BaseViewModel<UiState, UiAction, UiEffect>(
-        initialState = UiState()
-    ) {
+@HiltViewModel
+class CategoryDetailsViewModel @Inject constructor(
+    private val placeRepository: PlaceRepository,
+    savedStateHandle: SavedStateHandle,
+) : BaseViewModel<UiState, UiAction, UiEffect>(
+    initialState = UiState()
+) {
+
+    private val categoryId: Int = savedStateHandle["categoryId"] ?: 0
+
+    init {
+        getCategoryPlaces()
+    }
+
     override fun handleAction(action: UiAction) {
         when (action) {
-            UiAction.OnDetailsClick -> { setEffect(UiEffect.NavigateToDetails) }
+            is UiAction.OnDetailsClick -> {
+                setEffect(UiEffect.NavigateToDetails(action.placeId))
+            }
 
             UiAction.OnToggleFavorite -> {
                 setState { copy(isFavorite = !isFavorite) }
@@ -19,6 +37,28 @@ class CategoryDetailsViewModel :
 
             UiAction.OnBackClick -> {
                 setEffect(UiEffect.NavigateToBack)
+            }
+        }
+    }
+
+    private fun getCategoryPlaces() = viewModelScope.launch {
+        setState { copy(isLoading = true) }
+
+        runCatching {
+            placeRepository.getCategoryPlaces(categoryId)
+        }.onSuccess { places ->
+            setState {
+                copy(
+                    places = places,
+                    numberOfPlaces = places.size,
+                    isLoading = false,
+                )
+            }
+        }.onFailure {
+            setState {
+                copy(
+                    isLoading = false,
+                )
             }
         }
     }
