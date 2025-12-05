002ed8b81d30b535aeb2ceac3e80e96a5f0a672f
Berat Baran <berat_2002_40@hotmail.com>
2025-12-05 16:22:42 +0300

    feat: Update FavoritesContract to use PlaceModel and enhance navigation with placeId

--- app/src/main/java/com/beratbaran/loopa/ui/favorites/FavoritesContract.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/favorites/FavoritesContract.kt
@@ -1,17 +1,19 @@
 package com.beratbaran.loopa.ui.favorites
 
+import com.beratbaran.loopa.ui.homepage.PlaceModel
+
 object FavoritesContract {
     data class UiState(
         val isLoading: Boolean = false,
-        val favorites: List<FavoritesModel> = emptyList(),
+        val favorites: List<PlaceModel> = emptyList(),
     )
 
     sealed class UiEffect {
-        data object NavigateToDetails : UiEffect()
+        data class NavigateToDetails(val placeId: Int) : UiEffect()
     }
 
     sealed class UiAction {
-        data object OnDetailsClick : UiAction()
+        data class OnDetailsClick(val placeId: Int) : UiAction()
         data object OnUnFavoriteClick : UiAction()
     }
 }
