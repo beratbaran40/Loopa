e9dfd82967f9378f12a745628e5e8dcbf0cd432c
Berat Baran <berat_2002_40@hotmail.com>
2025-12-05 16:16:55 +0300

    feat: Add UserResponseDto and update UserDto structure

--- app/src/main/java/com/beratbaran/loopa/data/model/UserDto.kt
+++ app/src/main/java/com/beratbaran/loopa/data/model/UserDto.kt
@@ -1,9 +1,15 @@
 package com.beratbaran.loopa.data.model
 
+data class UserResponseDto(
+    val code: Int,
+    val message: String,
+    val data: UserDto,
+)
+
 data class UserDto(
+    val uid: String,
+    val email: String,
     val name: String?,
     val surname: String?,
     val fullName: String?,
-    val email: String,
-    val password: String,
 )
