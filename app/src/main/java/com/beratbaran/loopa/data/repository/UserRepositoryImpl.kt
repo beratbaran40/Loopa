220df99024c2b4507c5e803cc3ffdd9cdb7f8330
Berat Baran <berat_2002_40@hotmail.com>
2025-12-05 16:16:24 +0300

    chore: Bump Android Gradle Plugin version to 8.13.1

--- app/src/main/java/com/beratbaran/loopa/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/beratbaran/loopa/data/repository/UserRepositoryImpl.kt
@@ -1,9 +1,12 @@
 package com.beratbaran.loopa.data.repository
 
+import android.util.Log
 import com.beratbaran.loopa.data.model.LoginRequest
 import com.beratbaran.loopa.data.model.RegisterRequest
 import com.beratbaran.loopa.data.remote.api.LoopaApi
+import com.beratbaran.loopa.domain.UserModel
 import com.beratbaran.loopa.domain.repository.UserRepository
+import com.beratbaran.loopa.domain.toDomain
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
@@ -11,6 +14,7 @@ import javax.inject.Inject
 class UserRepositoryImpl @Inject constructor(
     private val firebaseAuth: FirebaseAuth,
     private val loopaApi: LoopaApi,
+    private var currentUser: UserModel? = null,
 ) : UserRepository {
 
     override suspend fun registerUser(
@@ -51,4 +55,26 @@ class UserRepositoryImpl @Inject constructor(
             Result.failure(e)
         }
     }
+
+    override suspend fun loadCurrentUser(): Result<UserModel> {
+        return try {
+            val firebaseUser = firebaseAuth.currentUser
+                ?: throw Exception("User not logged in")
+
+            val uid = firebaseUser.uid
+            Log.d("UserRepositoryImpl", "loadCurrentUser uid : $uid")
+
+            val response = loopaApi.getUser(uid)
+
+            val userModel = response.data.toDomain()
+
+            currentUser = userModel
+
+            Result.success(userModel)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    override fun getCurrentUser(): UserModel? = currentUser
 }
