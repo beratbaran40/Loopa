a329010a87434ed1d230a3a6bb3f09081615ddef
Berat Baran <berat_2002_40@hotmail.com>
2025-12-05 16:19:51 +0300

    feat: Update PlaceModel to make imageUrl and description nullable, and change rating to Double

--- app/src/main/java/com/beratbaran/loopa/ui/homepage/PlaceModel.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/homepage/PlaceModel.kt
@@ -3,8 +3,10 @@ package com.beratbaran.loopa.ui.homepage
 data class PlaceModel(
     val name: String,
     val location: String,
-    val imageUrl: String,
-    val rating: String,
+    val imageUrl: String?,
+    val rating: Double,
     val isFavorite: Boolean,
     val id: Int,
+    val description: String?,
+    val categoryId: Int?,
 )
