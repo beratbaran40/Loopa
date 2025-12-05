c174bf5d6e2de136e9b141664a28213c72ebbcf5
Berat Baran <berat_2002_40@hotmail.com>
2025-12-05 16:21:33 +0300

    feat: Enhance HomepageViewModel to load places and handle errors

--- app/src/main/java/com/beratbaran/loopa/ui/homepage/HomepageViewModel.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/homepage/HomepageViewModel.kt
@@ -1,17 +1,56 @@
 package com.beratbaran.loopa.ui.homepage
 
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.beratbaran.loopa.data.repository.PlaceRepository
 import com.beratbaran.loopa.ui.base.BaseViewModel
 import com.beratbaran.loopa.ui.homepage.HomepageContract.UiAction
-import com.beratbaran.loopa.ui.homepage.HomepageContract.UiState
 import com.beratbaran.loopa.ui.homepage.HomepageContract.UiEffect
+import com.beratbaran.loopa.ui.homepage.HomepageContract.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class HomepageViewModel : BaseViewModel<UiState, UiAction, UiEffect>(
+@HiltViewModel
+class HomepageViewModel @Inject constructor(
+    private val placeRepository: PlaceRepository,
+) : BaseViewModel<UiState, UiAction, UiEffect>(
     initialState = UiState()
 ) {
+
+    init {
+        getHomePagePlaces()
+        Log.d("HomepageViewModel", "init")
+    }
+
     override fun handleAction(action: UiAction) {
         when (action) {
-            UiAction.OnDetailsClick -> setEffect(UiEffect.NavigateToDetails)
+            is UiAction.OnDetailsClick -> setEffect(UiEffect.NavigateToDetails(action.placeId))
             UiAction.ToggleFavorite -> setEffect(UiEffect.NavigateToFavorites)
+        }
+    }
+
+    private fun getHomePagePlaces() = viewModelScope.launch {
+        Log.d("HomepageViewModel", "getHomePagePlaces")
+        setState { copy(isLoading = true) }
+
+        runCatching {
+            placeRepository.getPlaces()
+        }.onSuccess { places ->
+            Log.d("HomepageViewModel", "onSuccess")
+            Log.d("HomepageViewModel", "onSuccess top = ${places.topPlaces.size}")
+            Log.d("HomepageViewModel", "onSuccess want = ${places.wantToLookPlaces.size}")
+            setState {
+                copy(
+                    isLoading = false,
+                    topPlaces = places.topPlaces,
+                    wantToLookPlaces = places.wantToLookPlaces,
+                    error = null
+                )
+            }
+        }.onFailure { throwable ->
+            Log.e("HomepageViewModel", "Error: ${throwable.message}")
+            setState { copy(isLoading = false, error = throwable.message ?: "Something went wrong :(") }
         }
     }
 }
