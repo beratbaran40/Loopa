f6e889d5df749baf1f04346ae3dbc2699bfc8faa
Berat Baran <berat_2002_40@hotmail.com>
2025-12-05 16:22:19 +0300

    feat: Update FavoritesScreenPreviewProvider to use PlaceModel for favorites

--- app/src/main/java/com/beratbaran/loopa/ui/favorites/FavoritesScreenPreviewProvider.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/favorites/FavoritesScreenPreviewProvider.kt
@@ -8,51 +8,51 @@ class FavoritesScreenPreviewProvider : PreviewParameterProvider<FavoritesContrac
             FavoritesContract.UiState(
                 isLoading = true,
             ),
-            FavoritesContract.UiState(
-                favorites = listOf(
-                    FavoritesModel(
-                        name = "Eiffel Tower",
-                        location = "France",
-                        imageUrl = "",
-                        rating = "4.8",
-                        isFavorite = true,
-                    ),
-                    FavoritesModel(
-                        name = "Bryce Canyon National Park",
-                        location = "İstanbul",
-                        imageUrl = "",
-                        rating = "4.7",
-                        isFavorite = true,
-                    ),
-                    FavoritesModel(
-                        name = "Maiden’s Tower",
-                        location = "İstanbul",
-                        imageUrl = "",
-                        rating = "4.6",
-                        isFavorite = true,
-                    ),
-                    FavoritesModel(
-                        name = "Yerebatan Sarnıcı",
-                        location = "İstanbul",
-                        imageUrl = "",
-                        rating = "4.1",
-                        isFavorite = true,
-                    ),
-                    FavoritesModel(
-                        name = "Grand Canyon",
-                        location = "USA",
-                        imageUrl = "",
-                        rating = "4.0",
-                        isFavorite = true,
-                    ),
-                    FavoritesModel(
-                        name = "Mısır Çarşısı",
-                        location = "İstanbul",
-                        imageUrl = "",
-                        rating = "3.6",
-                        isFavorite = true,
-                    ),
-                )
-            )
+//            FavoritesContract.UiState(
+//                favorites = listOf(
+//                    PlaceModel(
+//                        name = "Eiffel Tower",
+//                        location = "France",
+//                        imageUrl = "",
+//                        rating = "4.8",
+//                        isFavorite = true,
+//                    ),
+//                    FavoritesModel(
+//                        name = "Bryce Canyon National Park",
+//                        location = "İstanbul",
+//                        imageUrl = "",
+//                        rating = "4.7",
+//                        isFavorite = true,
+//                    ),
+//                    FavoritesModel(
+//                        name = "Maiden’s Tower",
+//                        location = "İstanbul",
+//                        imageUrl = "",
+//                        rating = "4.6",
+//                        isFavorite = true,
+//                    ),
+//                    FavoritesModel(
+//                        name = "Yerebatan Sarnıcı",
+//                        location = "İstanbul",
+//                        imageUrl = "",
+//                        rating = "4.1",
+//                        isFavorite = true,
+//                    ),
+//                    FavoritesModel(
+//                        name = "Grand Canyon",
+//                        location = "USA",
+//                        imageUrl = "",
+//                        rating = "4.0",
+//                        isFavorite = true,
+//                    ),
+//                    FavoritesModel(
+//                        name = "Mısır Çarşısı",
+//                        location = "İstanbul",
+//                        imageUrl = "",
+//                        rating = "3.6",
+//                        isFavorite = true,
+//                    ),
+//                )
+//            )
         )
 }
