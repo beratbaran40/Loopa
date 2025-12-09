983c4030b409b81b8038fb7925a455f133a21c19
Berat Baran <berat_2002_40@hotmail.com>
2025-12-09 09:12:13 +0300

    feat: Update ProfileAvatar to use loopa image and adjust layout sizing

--- app/src/main/java/com/beratbaran/loopa/ui/profile/ProfileAvatar.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/profile/ProfileAvatar.kt
@@ -5,18 +5,15 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
@@ -39,12 +36,11 @@ fun ProfileAvatar(
                 .clickable { }
         ) {
             Image(
-                painter = painterResource(R.drawable.ic_avatar),
-                contentDescription = "Profile Avatar",
                 modifier = Modifier
-                    .size(120.dp)
-                    .clip(CircleShape),
-                contentScale = ContentScale.Crop,
+                    .height(240.dp)
+                    .aspectRatio(1f),
+                painter = painterResource(id = R.drawable.loopa),
+                contentDescription = null,
             )
         }
 
