1833ef3d17d14461f8f57525a2dee3b9da7b6509
Berat Baran <berat_2002_40@hotmail.com>
2025-12-09 09:12:54 +0300

    fix: Remove unnecessary Spacer from ProfileScreen UI

--- app/src/main/java/com/beratbaran/loopa/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/profile/ProfileScreen.kt
@@ -80,7 +80,6 @@ fun ProfileScreen(
             },
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Spacer(modifier = Modifier.height(8.dp))
 
         ProfileHeader(
         )
