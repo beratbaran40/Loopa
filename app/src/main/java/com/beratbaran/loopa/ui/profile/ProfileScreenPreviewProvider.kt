b5f8f8a3d076afe8be88cdf61efbad1b6a431e36
Berat Baran <berat_2002_40@hotmail.com>
2025-12-10 14:58:56 +0300

    feat: Update ProfileScreenPreviewProvider with fullName and removed errorMessage field

--- app/src/main/java/com/beratbaran/loopa/ui/profile/ProfileScreenPreviewProvider.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/profile/ProfileScreenPreviewProvider.kt
@@ -10,23 +10,23 @@ override val values: Sequence<ProfileContract.UiState>
             isLoading = true
         ),
         ProfileContract.UiState(
+            fullName = "Berat Baran",
             name = "Berat",
             surname = "Baran",
             email = "berat.baran@gmail.com",
             password = "7890",
             isLoading = false,
-            errorMessage = "",
             isSaveEnabled = false,
             isInEditMode = true
         ),
         ProfileContract.UiState(
+            fullName = "John Doe",
             name = "John",
             surname = "Doe",
             email = "test@example.com",
             password = "123456",
             isLoading = false,
             passwordStrength = PasswordStrength.WEAK,
-            errorMessage = "",
             isSaveEnabled = true
         ),
     )
