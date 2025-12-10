ed4bc40035d5f98394a27da03817888c74f3ede8
Berat Baran <berat_2002_40@hotmail.com>
2025-12-10 15:14:27 +0300

    feat: Add ChangePasswordButton component for UI interaction

---
+++ app/src/main/java/com/beratbaran/loopa/ui/changePasswordScreen/ChangePasswordButton.kt
@@ -0,0 +1,113 @@
+package com.beratbaran.loopa.ui.changePasswordScreen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.beratbaran.loopa.R
+import com.beratbaran.loopa.ui.changePasswordScreen.ChangePasswordContract.UiAction
+import com.beratbaran.loopa.ui.theme.LoopaTheme
+
+@Composable
+fun ChangePasswordButton(
+    onAction: (UiAction) -> Unit,
+    isInEditMode: Boolean,
+) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+    ) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .padding(start = 24.dp, end = 24.dp, bottom = 20.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Row(
+                modifier = Modifier
+                    .height(56.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(56.dp),
+                    onClick = { onAction(UiAction.OnEditPasswordClick) },
+                    shape = RoundedCornerShape(28.dp),
+                    //enabled = (isInEditMode && !areTextFieldsEmpty),
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        contentColor = MaterialTheme.colorScheme.background,
+                    ),
+                ) {
+                    Text(
+                        text = stringResource(R.string.profile_screen_change_password_profile_text),
+                        style = MaterialTheme.typography.bodyLarge,
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Icon(
+                        painter = painterResource(R.drawable.ic_edit_profile),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+                if (isInEditMode) {
+                    Spacer(modifier = Modifier.width(12.dp))
+
+                    IconButton(
+                        onClick = { onAction(UiAction.OnCancelChangesClick) },
+                        modifier = Modifier
+                            .size(56.dp)
+                            .clip(RoundedCornerShape(25.dp))
+                            .background(color = MaterialTheme.colorScheme.error),
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_cancel),
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.background,
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun ChangePasswordButtonPreview() {
+    LoopaTheme {
+        ChangePasswordButton(
+            onAction = {},
+            isInEditMode = true,
+        )
+    }
+}
