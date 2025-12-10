8eb9db671cbd108b5789d8de80ff7181676ca5d7
Berat Baran <berat_2002_40@hotmail.com>
2025-12-10 15:00:19 +0300

    feat: Update HomepageViewModel to load user name for greeting

--- app/src/main/java/com/beratbaran/loopa/ui/homepage/HomepageViewModel.kt
+++ app/src/main/java/com/beratbaran/loopa/ui/homepage/HomepageViewModel.kt
@@ -3,6 +3,7 @@ package com.beratbaran.loopa.ui.homepage
 import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.beratbaran.loopa.data.repository.PlaceRepository
+import com.beratbaran.loopa.domain.repository.UserRepository
 import com.beratbaran.loopa.ui.base.BaseViewModel
 import com.beratbaran.loopa.ui.homepage.HomepageContract.UiAction
 import com.beratbaran.loopa.ui.homepage.HomepageContract.UiEffect
@@ -14,11 +15,13 @@ import javax.inject.Inject
 @HiltViewModel
 class HomepageViewModel @Inject constructor(
     private val placeRepository: PlaceRepository,
+    private val userRepository: UserRepository
 ) : BaseViewModel<UiState, UiAction, UiEffect>(
     initialState = UiState()
 ) {
 
     init {
+        loadSaluteName()
         getHomePagePlaces()
         Log.d("HomepageViewModel", "init")
     }
@@ -51,6 +54,15 @@ class HomepageViewModel @Inject constructor(
         }.onFailure { throwable ->
             Log.e("HomepageViewModel", "Error: ${throwable.message}")
             setState { copy(isLoading = false, error = throwable.message ?: "Something went wrong :(") }
+        }
+    }
+
+    private fun loadSaluteName(){
+        viewModelScope.launch {
+            userRepository.loadCurrentUser()
+                .onSuccess { user ->
+                    setState { copy(name = user.name) }
+                }
         }
     }
 }
